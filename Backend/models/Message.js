c88d0b95b7bcc53c1190c1bc1dea606c435de2e8
pierpaolo <pier.paolo.cann.one90@gmail.com>
2026-05-21 20:18:53 +0200

    preparazione message.js

--- Backend/models/Message.js
+++ Backend/models/Message.js
@@ -1,3 +1,10 @@
 // Essenziale per la chat real-time. 
 // Memorizza chi invia il messaggio, chi lo riceve e il testo. 
 // Ti permetterà di salvare permanentemente lo storico delle chat gestite tramite Socket.IO.
+
+
+const moongose = require('mongoose');
+
+const MessageSchema = new.moongose.MessageSchema({
+    
+})
