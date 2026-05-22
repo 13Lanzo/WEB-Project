4b95459948b35be5c8dbf264ae45f1c6d40ac701
pierpaolo <pier.paolo.cann.one90@gmail.com>
2026-05-22 22:55:41 +0200

    sistemato rotta registrazioneUtente

--- Backend/routes/registrazioneUtente.js
+++ Backend/routes/registrazioneUtente.js
@@ -3,93 +3,109 @@
 
 const express = require('express');
 const router = express.Router();
-const User = require('../models/User');
+const User = require('../models/User'); //importa il modello User per interagire con il DB
 
 //Metodo POST per la registrazione di un nuovo utente
 
-router.post('/register', async(req,res) => {
-    try{
-        const { nome, email, password, eta, ruolo, tagPreferenze, bio } = req.body;
+router.post('/', async (req, res) => {
+    try {
+        const {nome, email, password, eta, ruolo, tagPreferenze, bio} = req.body;
 
-        //controllo se l'utente esiste già
-        const utenteEsistente = await User.findOne({ email });
+        //verifichiamo se l'utente esiste già
+        const utenteEsistente = await User.findOne({email: req.body.email});
         if(utenteEsistente){
             return res.status(400).json({
                 success: false,
                 messaggio: "L'email è già registrata."
-            })
-        };
-        //se dopo il controllo l'utente non risulta registrato, crea un nuovo utente
-        const nuovoUtente = new User({
-            nome, email, password, eta, ruolo, tagPreferenze, bio
-        });
+            });
+        } 
+        
+        //creiamo un nuovo utente 
+
+        const nuovoUtente = new User({nome, email, password, eta, ruolo, tagPreferenze, bio});
         await nuovoUtente.save();
-        res.status(201).json({
+
+        return res.status(201).json({
             success: true,
-            messaggio:"Utente registrato con successo!"
+            messaggio: "Utente registrato con successo!",
+            dati: nuovoUtente
         });
-    } catch(err){
-        res.status(500).json({success:false, errore: err.message});
+
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            messaggio: "Si è verificato un errore interno al server.",
+            dettaglio: err.message
+        });
     }
 });
 
-//Metodo GET per ottenere tutte le informazioni di un utente
-
-router.get('/utenti', async(req,res) => {
+//metodo GET per ottenere tutte le informazioni di un utente
+router.get('/', async (req, res) => {
     try{
-        //trovo gli utenti nel db
-        const utenti = await User.find();
+        const utenti= await User.find();
         res.status(200).json({
             success: true,
             dati: utenti
         });
-    }catch(err){
-        res.status(500).json({success:false, errore: err.message});
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            messaggio: "Si è verificato un errore interno al server",
+            dettaglio: err.message
+        });
     }
 });
 
-//Metodo UPDATE per modificare le informazioni di un utente
+//metodo UPDATE per modificare le informazioni di un utente
 
-router.put('/update/:id', async(req,res) => {
-    try{
-        //cerca un utente tramite l'id e aggiorna le informazioni in base alla req
+router.put('/:id', async (req, res) => {
+    try {
         const utenteAggiornato = await User.findByIdAndUpdate(req.params.id, req.body, {new: true});
-
         if(!utenteAggiornato){
-            return res.status(404).json({
-                success:false,
-                messaggio: "Utente non trovato."
-            })
-        };
-        res.status(200).json({
-            success: true,
-            messaggio: "Utente aggiornato con successo!",
-            dati: utenteAggiornato
+            res.status(404).json({
+                success: false,
+                messaggio: "Impossibile aggiornare l'utente"
+            });
+        } else {
+            res.status(200).json({
+                success: true,
+                messaggio: "Utente aggiornato con successo!",
+                dati: utenteAggiornato
+            });
+        }
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            messaggio: "Si è verificato un errore interno al server",
+            dettaglio: err.message
         });
-    }catch(err){
-        res.status(500).json({success:false, errore: err.message});
     }
 });
 
-//Metodo DELETE per eliminare un utente 
-/*
-router.delete('/delete/:id', async(req, res) => {
+//metodo DELETE per eliminare un utente
+
+router.delete('/:id', async (req, res) => {
     try{
-        //cerca un utente tramite l'id e lo elimina
-        const utenteEliminato = await User.findByIdAndDelete(req.params.id);
+        const utenteEliminato = await User.findByIdAndRemove(req.params.id);
         if(!utenteEliminato){
-            return res.status(404).json({
-                success:false,
-                messaggio: "Utente non trovato"
-            })
-        };
-        res.status(200).json({
-            success: true,
-            messaggio: "Utente eliminato con successo!"
+            res.status(404).json({
+                success: false,
+                messaggio: "Impossibile eliminare l'utente"
+            });
+        } else {
+            res.status(200).json({
+                success: true,
+                messaggio: "Utente eliminato con successo!"
+            });
+        }
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            messaggio: "Si è verificato un errore interno al server",
+            dettaglio: err.message
         });
-    }catch(err){
-        res.status(500).json({success:false, errore:err.message});
     }
 });
-*/
+
 module.exports = router;
