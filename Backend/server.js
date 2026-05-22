a6a047db4971c999dcb1eb920109e0ab670fdb39
pierpaolo <pier.paolo.cann.one90@gmail.com>
2026-05-22 20:45:15 +0200

    sistemato connessione al server e database mongoose

--- Backend/server.js
+++ Backend/server.js
@@ -1,3 +1,5 @@
+/*
+
 //punto di ingresso dell'applicazione
 
 //1. carica le variabili dal file .env all'avvio del processo
@@ -50,3 +52,41 @@ app.listen(PORT, () => {
     console.log(`Server in ascolto sulla porta ${PORT}...`);
     console.log('Testa la rotta su http://localhost:' + PORT + '/')
 })
+*/
+
+require('dotenv').config(); //carica le variabili dal file .env
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+app.use(express.json()); //permette di leggere i dati JSON inviati dalle richieste 
+
+//impostiamo le rotte
+const registerUserRoute = require('./routes/registrazioneUtente'); 
+const loginUserRoute = require('./routes/login');
+
+app.use('/registrazione', registerUserRoute); //tutte le rotte di registrazioneUtente.js inizieranno con /registrazione
+app.use('/login', loginUserRoute); //tutte le rotte di login.js inizieranno con /login
+
+//verifichiamo la rotta di test
+app.get('/', (req, res) => {
+    res.status(200).json({messaggio: "Server backend attivo e funzionante!"});
+});
+
+//connettiamo il db
+
+const MONGODB_URI = process.env.MONGODB_URI;
+mongoose.connect(MONGODB_URI)
+    .then(() => {
+        console.log("Connesso correttamente a MongoDB Atlas!")
+    })
+    .catch((err) => {
+        console.error("Errore di connessione a MongoDB Atlas:", err.message)
+    });
+
+//avvio del server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Il server è in ascolto sulla porta ${PORT}...`);
+    console.log(`Testa la rotta su http://localhost:${PORT}/`);
+});
