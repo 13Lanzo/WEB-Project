f7967bed6a8dc2ca52a870320e88497a106826ef
Giuseppe Lanzolla <g.lanzolla2@studenti.poliba.it>
2026-05-22 08:50:07 +0200

    Creazione DB completata

--- Backend/models/Message.js
+++ Backend/models/Message.js
@@ -6,5 +6,27 @@
 const moongose = require('mongoose');
 
 const MessageSchema = new.moongose.MessageSchema({
-    
+    mittente: {
+        type: moongose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    destinatario: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    testo: {
+        type: String,
+        required: true,
+        trim: true,
+        maxlength: 1000
+    },
+    letto: {
+        type: Boolean,
+        default: false
+    }, 
+    timestamps: true // Specifica data e ora esatta dell'invio del messaggio
 })
+
+module.exports = mongoose.model('Message', MessageSchema);
