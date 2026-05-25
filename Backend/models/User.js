a0a6cc9336ebadbf0d16bb5d384edbf82f54284d
Giuseppe Lanzolla <g.lanzolla2@studenti.poliba.it>
2026-05-25 16:00:07 +0200

    modificata User.js con bcrypt

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -59,8 +59,25 @@ const UserSchema = new mongoose.Schema({
     timestamps: true // Aggiunge automaticamente createdAt e updatedAt nel database
 });
 
-//cifratura della password prima del salvataggio nel DB
+// Cifratura della password prima del salvataggio nel database
+UserSchema.pre('save', async function (next) {
+    //Esegui la cifratura solo se la password è stata modificata (o è nuova)
+    if (!this.isModified('password')) return next();
 
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        // genera hash diverse per utenti che hanno la stessa password
 
+        next();
+    } catch (err) {
+        next(err);
+    }
+});
+
+// Metodo di istanza per verificare la password inserita con quella cifrata
+UserSchema.methods.comparePassword = async function(passwordInserita) {
+    return await bcrypt.compare(passwordInserita, this.password);
+};
 
 module.exports = mongoose.model('User', UserSchema);
