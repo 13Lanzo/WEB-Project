ced258152695bef12259a0fe2a957e3b441148d5
Francesca <f.cioccariello@studenti.poliba.it>
2026-06-15 13:47:43 +0200

    footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -10,7 +10,7 @@ const Footer= () => {
         <div className='footerBrand'>
           <h2 className='Title'>Room4U</h2>
           <p className='brandText'>
-            Piattaforma per trovare coinquilini più bella del west
+            L'innovativa piattaforma per trovare la tua stanza ideale in base ai tuoi interessi!
           </p>
         </div>
 
